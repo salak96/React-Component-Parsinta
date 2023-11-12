3b619d47c4f24201a4ff1ab460c36e6f0d99f68c
salak96 <lambangsasangka@gmail.com>
2023-11-12 21:44:16 +0700

    useState Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import Card from './components/Card';
+import React, { useState } from 'react';
+import PlaceContentCenter from './components/PlaceContentCenter';
 import Button from './components/Button';
 
 // Defining a functional component named MyFunctionComponent
 export default function App() {
+    const [name, setName] = useState('sasangka');
+    const [counter, setCounter] = useState(0);
+    function handleClick() {
+        const nextName = 'kurniawan';
+        setName(nextName);
+        console.log(name, nextName);
+    }
     return (
-        <div className='bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'>   
-            <div className='max-w-md w-full flex items-center gap-4'>
-                <Card>
-                <Card.Title>Title</Card.Title>
-                <Card.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.</Card.Body>
-                <Card.Footer>
-                    <Button>Click me</Button>
-                </Card.Footer>
-                </Card>
-           </div>
-        </div>
+        <PlaceContentCenter>
+            <div className='flex flex-col justify-center items-center'>
+                {name}
+                <Button onClick={handleClick}>Change Name</Button>
+                <h1>{counter}</h1>
+                <div className="mt-5 flex gap-2">
+                    <Button className="bg-red-600" onClick={() => setCounter(counter + 1)}>+</Button>
+                    <Button className="bg-yellow-600" onClick={() => setCounter(counter - 1)}>-</Button>
+                </div>
+            </div>
+        </PlaceContentCenter>
     );
 }
---
+++ src/components/PlaceContentCenter.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+export default function PlaceContentCenter({children}) {
+  return (
+    <div className='bg-black'>
+        <div className='text-violet-50 bg-violet-900/20 min-h-screen flex items-center justify-center antialiased tracking-tight'>
+            <div className='max-w-lg w-full'>
+                {children}
+            </div>
+        </div>
+    </div>
+  );
+}
