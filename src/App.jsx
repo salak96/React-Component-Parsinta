eed168343b6790cdff382bdf8eec92ec4200478d
salak96 <lambangsasangka@gmail.com>
2023-11-12 16:20:23 +0700

    className default css tailwinds props

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,23 @@
 import React from 'react';
 // Importing the custom Button component and two icons from the '@tabler/icons-react' library
 import Button from './components/Button';
-import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from '@tabler/icons-react';
 
 // Defining a functional component named MyFunctionComponent
 export default function MyFunctionComponent() {
     return (
         // Main container with background color and centering styles
         <div className='bg-slate-500 grid place-content-center min-h-screen'>
-            {/* Container for buttons with horizontal gap */}
             <div className='flex gap-x-2'>
-                {/* Using the custom Button component with an onClick handler and type='submit' */}
-                <Button onClick={() => console.log('register')} type='submit'>
-                    {/* IconBrandFacebook component and the text 'Register' */}
+                <Button className='bg-red-500 hover:bg-black' onClick={() => console.log('register')} type='submit'>
                     <IconBrandFacebook /> Register
                 </Button>
-                {/* Another instance of the custom Button component with an onClick handler and type='button' */}
-                <Button onClick={() => console.log('Login')} type='button'>
-                    {/* IconBrandTwitter component and the text 'Login' */}
+                <Button className='bg-yellow-500 hover:bg-green-600' onClick={() => console.log('Login')} type='button'>
                     <IconBrandTwitter />
+                    Login
+                </Button>
+                <Button>
+                    <IconBrandGithub />
                     Login
                 </Button>
             </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 // Defining a functional component named Button that takes props as its parameter
 export default function Button(props) {
     
-    // Destructuring props to extract 'children' and 'text'
-    const { children, text } = props; 
+    // Destructuring props to extract 'children' and 'text' default clasName Tailwind css
+    const { children, text,className='bg-blue-600 hover:bg-blue-700' } = props; 
 
     // Returning a button element with some styles and classes
     return (
         // Using spread operator to pass all props to the button element
-        <button {...props} className='[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-md'>
+        <button {...props} className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center text-white font-bold py-2 px-4 rounded`}>
             {/* Displaying either the 'text' prop or 'children' prop */}
             {text || children}
         </button>
