8eb5351efa746b1c80f21da2f58911893ac39eb9
salak96 <lambangsasangka@gmail.com>
2023-11-13 14:13:36 +0700

    function form

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,41 @@ import Label from './components/Label';
 
 // Defining a functional component named MyFunctionComponent
 export default function App() {
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+    });
+
+    function onChange(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }
+    function submit(e){
+        e.preventDefault();
+        console.log(form)
+    }
     return (
         <PlaceContentCenter>
             <Card>
                 <Card.Title>Sign up for new account</Card.Title>
+                <form onSubmit={submit}>
                 <Card.Body>
+                    <div className='mb-5 border rounded-lg'>
+                        <p>Name : {form.name || 'No name provided'}</p> <p>Email : {form.email || 'No email provided'}</p>
+                    </div>
                     <div className='mb-6'>
                         <Label htmlFor='name' value={'Name'}>
                             Name
                         </Label>
-                        <Input id='name' placeholder='Enter your name' />
+                        <Input id='name' type='text' name='name' value={form.name} onChange={onChange} placeholder='Enter your name' />
                     </div>
                     <div className='mb-6'>
                         <Label htmlFor='email' value={'Email'}>
                             Email
                         </Label>
-                        <Input id='email' placeholder='Enter your Email' />
+                        <Input type='email' name='email' value={form.email} onChange={onChange} id='email' placeholder='Enter your Email' />
                     </div>
                 </Card.Body>
                 <Card.Footer>
@@ -33,6 +52,7 @@ export default function App() {
                         Github
                     </Button>
                 </Card.Footer>
+                </form>
             </Card>
         </PlaceContentCenter>
     );
